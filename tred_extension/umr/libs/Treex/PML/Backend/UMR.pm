0f66b6d92dfe8ecb215bd3c7ffa7ef8fa1e6c86a
Jan Štěpánek <jan.stepanek@matfyz.cz>
2026-02-15 01:52:32 +0100

    Parse document-level graph in (b)tred
    Be stricter in the parser, report errors in missing closing brackets
    in the document-level. Also, no longer ignore modal and temporal edges.

--- tred_extension/umr/libs/Treex/PML/Backend/UMR.pm
+++ tred_extension/umr/libs/Treex/PML/Backend/UMR.pm
@@ -70,13 +70,14 @@ sub read {
             }
 
         } elsif (/# *alignment/) {
-            $mode = 'alignment';
+            if ('sentence' eq $mode) {
+                $mode = 'alignment';
+            } else {
+                die "Alignment not expected"
+            }
 
         } elsif ('alignment' eq $mode) {
-            if ("" eq $_) {
-                $mode = "";
-
-            } elsif (/^(\w+): ([-0-9, ]+)/) {
+            if (/^(\w+): *([-0-9, ]+)/) {
                 my ($id, $alignment) = ($1, $2);
                 my @ords = map {
                                my ($from, $to) = '-1--1' eq $_ ? (0, 0)
@@ -89,28 +90,82 @@ sub read {
                     $root->{words}[$ord]->set_value(PML::List(
                         PML::ListV($root->{words}[$ord]->value), $id));
                 }
+            } elsif ("" eq $_) {
+                $mode = 'before_document';
+            } else {
+                die 'Unparsable alignment'
             }
 
         } elsif (/# *document level annotation/) {
-            $mode = 'document';
-
-        } elsif ('document' eq $mode) {
-            if ("" eq $_) {
-                $mode = "";
-            } elsif (/\( (\w+) \s+ :same-(e(?:ntity|vent)) \s+ (\w+) \)/x) {
-                my ($target_id, $type, $source_id) = ($1, $2, $3);
-                if (my $source = (grep $_->{id} eq $source_id,
-                                  $root->descendants)[0]
-                ) {
-                    add_to_links($source, $target_id, "coref:$type");
-                } elsif (my $target = (grep $_->{id} eq $target_id,
-                                       $root->descendants)[0]
-                ) {
-                    add_to_links($target, $source_id, "coref:$type");
-                } else {
-                    die "Neither $source_id nor $target_id found";
-                }
+            if ('before_document' eq $mode) {
+                $mode = 'document_start';
+            } else {
+                die 'Document level not expected'
             }
+
+        } elsif ('document_start' eq $mode) {
+            if (m{^\(s[0-9]+s0 / sentence(\))?$}) {
+                $mode = $1 ? 'doc_end' : 'document';
+            } elsif ("" eq $_) {
+                $mode = 'doc_end';
+            } else {
+                die 'Sentence in document level expected.'
+            }
+
+        } elsif ($mode =~ /^document(?:_(?:modal|coref|temporal))?/) {
+            if ('document' eq $mode
+                && /^\s*(?: : (modal | coref | temporal) \s+ \( )? (.*)/x
+            ) {
+                $mode .= "_$1", $_ = $2 if $1;
+                next unless length $_;  # Newline after ":coref ("
+            }
+
+            if ($mode =~ /^document_(?:coref|modal|temporal)$/) {
+                if (/^\s* \( ([-\w]+) \s+
+                       # coref
+                       :(same-e(?:ntity|vent)|subset(?:-of)?|contains
+                       # temporal
+                       |depends-on|after|before|overlap|contained
+                       # modal
+                       |(?:(?:strong-)?
+                             (?:neutral|partial|full)-(?:affirmative|negative))
+                       |modal|unspecified)
+                     \s+ ([-\w]+) \) ( \)* ) $ /x
+                ) {
+                    my ($target_id, $type, $source_id, $final) = @{^CAPTURE};
+                    if ('document_coref' eq $mode) {
+                        add_relation($target_id, "coref:$type",
+                                     $source_id, $root, $doc);
+
+                    } elsif ('document_temporal' eq $mode) {
+                        add_relation($target_id, "temp:$type",
+                                     $source_id, $root, $doc);
+
+                    } elsif ('document_modal' eq $mode) {
+                        $target_id = $root->{id} if 'root' eq $target_id;
+                        add_relation($target_id, "modal:$type",
+                                     $source_id, $root, $doc, 1);
+
+                    } else {
+                        die "Unexpected part of document level annotation: '$_'"
+                    }
+                    if (1 == length $final) {
+                        $mode = 'document';
+                    } elsif (2 == length $final) {
+                        $mode = 'doc_end';
+                    }
+                } else {
+                    die "Invalid document level annotation $mode: $_"
+                }
+            } else {
+                die 'Coref, modal, or temporal expected'
+            }
+        } elsif ('doc_end' eq $mode) {
+            die "Unexpected '$_' after document end" if "" ne $_;
+
+            $mode = "";
+        } elsif (! /^(?:#|Index: [\s0-9]+$|$)/ && 'words' ne $mode) {
+            die "Unexpected in $mode: $_"
         }
         #warn "$mode: $_" if length $mode || length;
     }
@@ -126,6 +181,51 @@ sub write {
     return 1
 }
 
+
+{   my %ALLOWED_NON_NODES = (modal => {'have-condition'         => 'st',
+                                       'have-condition-91'      => 'st',
+                                       'null-conceiver'         => 't',
+                                       purpose                  => 'st',
+                                       author                   => 'st'},
+                             temp => {'document-creation-time'  => 'st',
+                                      'past-reference'          => 't',
+                                      'present-reference'       => 't',
+                                      root                      => 't'});
+    sub allowed_non_node {
+        my ($type, $value, $role) = @_;
+        my $short_type = $type =~ s/:.+//r;
+        return exists $ALLOWED_NON_NODES{$short_type}
+               && exists $ALLOWED_NON_NODES{$short_type}{$value}
+               && $ALLOWED_NON_NODES{$short_type}{$value} =~ /$role/
+    }
+}
+
+
+sub add_relation {
+    my ($target_id, $type, $source_id, $root, $doc, $include_roots) = @_;
+    my @nodes = $doc ? (map $_->descendants, $doc->trees)
+                     : $root->descendants;
+    push @nodes, $doc ? $doc->trees : $root if $include_roots;
+    my $source = (grep $_->{id} eq $source_id, @nodes)[0];
+    my $target = (grep $_->{id} eq $target_id, @nodes)[0];
+    warn "$type source $source_id not found"
+        if ! defined $source && ! allowed_non_node($type, $source_id, 's');
+    warn "$type target $target_id not found"
+        if ! defined $target && ! allowed_non_node($type, $target_id, 't');
+    if ($source) {
+        add_to_links($source, $target_id, $type);
+
+    } elsif ($target) {
+        add_to_links($target, $source_id, $type);
+
+    } elsif ($source_id =~ /^have-condition(?:-91)?/) {
+        warn "Condition";
+    } else {
+        die "Neither $source_id nor $target_id found ($type)"
+    }
+}
+
+
 sub new_root {
     my ($words, $sentence_index) = @_;
     'Treex::PML::Factory'->createTypedNode(
@@ -140,6 +240,7 @@ sub new_root {
             '#name' => 'sent',
     });
 }
+
 
 sub parse_sentence {
     my ($root, $buffer) = @_;
@@ -187,6 +288,7 @@ sub parse_sentence {
                 die "XXX!!! Can't parse:\n$$buffer"
             }
         }
+
     } else {
         die "Can't parse: $$buffer";
     }
@@ -200,6 +302,7 @@ sub parse_sentence {
     die "Unfinished sentence tree at line $..\n"
 }
 
+
 sub add_to_links {
     my ($node, $target_id, $type) = @_;
     $node->{links} //= 'Treex::PML::Factory'->createList();
@@ -208,6 +311,7 @@ sub add_to_links {
             $type, {'target.rf' => $target_id}));
 }
 
+
 sub add_to_features {
     my ($node, $relation, $value) = @_;
     $node->{features} //= 'Treex::PML::Factory'->createList();
@@ -215,5 +319,6 @@ sub add_to_features {
         'Treex::PML::Factory'->createContainer(
             $value, {type => $relation}));
 }
+
 
 __PACKAGE__
